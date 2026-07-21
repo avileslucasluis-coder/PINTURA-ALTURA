a67a7d3342c35896012ed638f045cb1af3f918a3
avileslucasluis-coder <avileslucasluis@gmail.com>
2026-07-21 12:06:49 -0500

    nuevos proyectos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -19,9 +19,9 @@ export const projectsData: ProjectData[] = [
     category: "Pintura Comercial",
     visible: true,
     images: [
-      "/uploads/mc-1.png",
-      "/uploads/mc-2.png",
-      "/uploads/mc-3.png"
+      "/public/mc-1.png",
+      "/public/mc-2.png",
+      "/public/mc-3.png"
     ]
   },
   {
