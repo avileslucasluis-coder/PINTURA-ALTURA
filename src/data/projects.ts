ac663726aa72d1aefbf020f49414c513eab22637
avileslucasluis-coder <avileslucasluis@gmail.com>
2026-07-24 22:52:00 -0500

    actualizacion de proyectos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -116,8 +116,11 @@ export const projectsData: ProjectData[] = [
     visible: true,
     images: [
       "/salinas1.jpg",
-      "/uploads/ejemplo3-b.jpg",
-      "/uploads/ejemplo3-c.jpg"
+      "/salinas 2.jpeg",
+      "/salinas 3.jpeg",
+      "/salinas 4.jpeg",
+      "/salinas 5.jpeg",
+      "/salinas 6.jpeg"
     ]
   },
    {
@@ -298,4 +301,228 @@ export const projectsData: ProjectData[] = [
       "/punt 8.jpg"
     ]
   },
+  {
+    id: "17",
+    title: "Pintura en los Condominios de Romadera  ",
+    location: "kilómetro 1.6 al 1.8 de la Avenida Narcisa de Jesús.",
+    date: "05 de junio, 2023",
+    description:  "Se realizó el trabajo de pintura exterior en los edificios del conjunto residencial Romadera, que aún se encontraba en etapa de acabados. Debido a la altura de las torres (5 pisos), el trabajo se ejecutó con técnica de rappel y cuerdas de seguridad. Se pintaron las fachadas siguiendo el diseño de cada bloque, combinando tonos café y beige, con detalles en rojo y naranja en los pasillos y áreas comunes. El equipo trabajó en varios puntos del edificio al mismo tiempo para cumplir con los tiempos de entrega, siempre bajo las medidas de seguridad necesarias para trabajar en altura." ,
+    category: "Pintura de Edificios",
+    visible: true,
+    images: [ 
+      "/rom 1.jpg",
+      "/rom 2.jpg",
+      "/rom 3.jpg",
+      "/rom 4.jpg",
+    ]
+  },
+  {
+    id: "18",
+    title: "Renovación de Fachada en Edificio - Ciudadela Entre Ríos",
+    location: "Ciudadela Entre Ríos",
+    date: "02 de marzo,2024",
+    description: "El trabajo realizado consistió en la renovación completa de la fachada de un edificio multifamiliar de tres pisos, que presentaba pintura deteriorada y agrietada, mediante preparación de superficie, sellado de fisuras y aplicación de pintura exterior.",
+    category: "Pintura de Edificios",
+    visible: true,
+    images: [
+      "/rios 1.jpg",
+      "/rios 2.jpg",
+      "/rios 3.jpg",
+      "/rios 4.jpg"
+    ]
+  },
+{
+    id: "19",
+    title: "Pintura Exterior en Edificios de la regeneracion urbana desde el cerro santa Ana, la gobernación y la zona bancaria ",
+    location: "Guayaquil",
+    date: "20 de julio, 2019",
+    description: "El trabajo realizado consistió en la pintura exterior de varios edificios ubicados en el sector de Guayaquil, mediante técnica de rappel con cuerdas de seguridad. Se trabajó en estructuras de distintas alturas y estilos arquitectónicos, incluyendo edificaciones patrimoniales con detalles ornamentales, cúpulas y molduras, cuidando el acabado en cada zona. El equipo trabajó en varios puntos de cada fachada al mismo tiempo, siempre bajo las medidas de seguridad necesarias para el trabajo en altura.",
+    category: "Pintura de Edificios",
+    visible: true,
+    images: [
+      "/urb 1.jpeg",
+      "/urb 2.jpeg",
+      "/urb 3.jpeg",
+      "/urb 4.jpeg",
+      "/urb 5.jpeg",
+      "/urb 6.jpeg",
+      "/urb 7.jpeg",
+      "/urb 8.jpeg",
+      "/urb 9.jpeg",
+      "/urb 10.jpeg",
+      "/urb 11.jpeg",
+      "/urb 12.jpeg",
+      "/urb 13.jpeg",
+      "/urb 14.jpeg",
+      "/urb 15.jpeg",
+      "/urb 16.jpeg",
+      "/urb 17.jpeg",
+      "/urb 18.jpeg",
+      "/urb 19.jpeg",
+      "/urb 20.jpeg",
+      "/urb 21.jpeg",
+      "/urb 22.jpeg",
+      "/urb 23.jpeg",
+      "/urb 24.jpeg",
+      "/urb 25.jpeg",
+      "/urb 26.jpeg",
+      "/urb 27.jpeg",
+      "/urb 28.jpeg",
+      "/urb 29.jpeg",
+      "/urb 30.jpeg",
+      "/urb 31.jpeg",
+      "/urb 32.jpeg",
+      "/urb 33.jpeg",
+      "/urb 34.jpeg",
+      "/urb 35.jpeg",
+      "/urb 36.jpeg",
+      "/urb 37.jpeg",
+      "/urb 38.jpeg",
+      "/urb 39.jpeg",
+      "/urb 40.jpeg",
+      "/urb 41.jpeg",
+    ]
+  },
+  {
+    id: "20",
+    title: "Pintura Exterior de Vivienda en La Alborada",
+    location: "Guayaquil",
+    date: "20 de febrero, 2018",
+    description: "El trabajo realizado consistió en la pintura exterior de una vivienda de dos plantas ubicada en el sector de La Alborada, Guayaquil. Se aplicó un cambio de color completo de la fachada (de tonos grises a un amarillo mostaza con detalles cafés en las columnas y molduras), incluyendo el sellado y preparación previa de la superficie de hormigón, protección de ventanas y elementos metálicos, y acabado final en marquesinas, cornisas y detalles decorativos.",
+    category: "Pintura Residencial",
+    visible: true,
+    images: [
+      "/alb 1.jpeg",
+      "/alb 2.jpeg",
+      "/alb 3.jpeg",
+      "/alb 4.jpeg"
+    ]
+    },
+    {
+    id: "21",
+    title: "Pintura interior en el Edificio Equilibrium",
+    location: "intersección de la Avenida Juan Tanca Marengo y la Avenida Joaquín Orrantia",
+    date: "20 de febrero, 2018",
+    description: "El trabajo realizado en el Edificio Equilibrium consistió en labores de mantenimiento y acceso en altura dentro de su atrio principal, utilizando una alta estructura de andamios modulares metálicos para permitir que los operarios alcanzaran la zona de los ventanales superiores y el techo. Esta intervención requirió una cuidadosa coordinación logística y de personal, con trabajadores posicionados tanto en la plataforma elevada como en las diferentes plantas y barandillas del edificio, facilitando así las tareas técnicas en este espacio de gran altura.",
+    category: "Pintura de Edificios",
+    visible: true,
+    images: [
+      "/equi 1.jpeg",
+      "/equi 2.jpeg"
+    ]
+    },
+    {
+    id: "22",
+    title: "Pintura exterior en el edificio de Ceibos",
+    location: "AV 47 NO CEIBOS",
+    date: "20 de febrero, 2022",
+    description: "El trabajo realizado consistió en el mantenimiento y renovación integral de la pintura de la fachada exterior del edificio ubicado en el sector de Ceibos. Como se evidencia en la secuencia de imágenes, la intervención se ejecutó mediante técnicas de trabajo en altura con descenso vertical (rapel), utilizando sistemas de seguridad conformados por arneses y cuerdas ancladas desde la azotea. Dos operarios realizaron las labores de mantenimiento, avanzando progresivamente desde los niveles superiores hasta los balcones y la planta baja. Los trabajos incluyeron la renovación del revestimiento de las paredes de color blanco y el tratamiento de la columna decorativa con acabado tipo piedra, logrando una restauración completa, uniforme y estéticamente renovada de las áreas visibles de la fachada del edificio.",
+    category: "Pintura de Edificios",
+    visible: true,
+    images: [
+      "/cei 1.jpeg",
+      "/cei 2.jpeg",
+      "/cei 3.jpeg",
+      "/cei 4.jpeg",
+      "/cei 5.jpeg",
+      "/cei 6.jpeg",
+      "/cei 7.jpeg",
+      "/cei 8.jpeg",
+      "/cei 9.jpeg"
+    ]
+    },
+    {
+    id: "23",
+    title: "Pintura exterior en el Condominio Genesis",
+    location: "calle Cacique Álvarez 528 y Huancavilca",
+    date: "20 de febrero, 2022",
+    description: "Se realizó el mantenimiento de la pintura de la fachada del Hotel Genesis, un edificio esquinero de cuatro plantas. El trabajo consistió en la renovación cromática de las franjas horizontales. Originalmente, las franjas alternaban entre un tono rojo oscuro y amarillo crema, con secciones en la planta baja de color blanco y beige. Tras la intervención, se mantuvo el color amarillo crema, pero las franjas rojas fueron reemplazadas por un tono rosado vibrante, logrando un aspecto más moderno y llamativo para el hotel. La aplicación se realizó de manera uniforme sobre la estructura existente, conservando la distribución de las ventanas y los equipos de aire acondicionado.",
+    category: "Pintura de Edificios",
+    visible: true,
+    images: [
+      "/ge 1.jpeg",
+      "/ge 2.jpeg",
+      "/ge 3.jpeg",
+      "/ge 4.jpeg",
+      "/ge 5.jpeg"
+    ]
+    },
+    {
+    id: "24",
+    title: "Pintura exterior en el Condominio",
+    location: "calle Dr. Héctor Romero #216 y la Av. Dr. José María García Moreno",
+    date: "20 de agosto, 2022",
+    description: "Se realizó el mantenimiento de la pintura de la fachada en el condominio, un edificio de varios pisos con grandes ventanales de vidrio oscurecido y estructura blanca. El trabajo consistió en la renovación y aplicación de pintura en las paredes exteriores mediante técnicas de descenso vertical con arneses y cuerdas, utilizando baldes con material mientras los operarios trabajaban suspendidos a lo largo de los niveles superiores e intermedios del inmueble.",
+    category: "Pintura de Edificios",
+    visible: true,
+    images: [
+      "/con 1.jpeg",
+      "/con 2.jpeg",
+      "/con 3.jpeg",
+      "/con 4.jpeg",
+      "/con 5.jpeg"
+    ]
+    },
+    {
+    id: "25",
+    title: "Pintura exterior en el Edificio El Astillero",
+    location: "Complejo Puerto Santa Ana, junto al río Guayas en la ciudad de Guayaquil.",
+    date: "20 de agosto, 2022",
+    description: "El trabajo realizado en el Edificio El Astillero consistió en labores de mantenimiento y renovación de la pintura de la fachada exterior, ejecutadas mediante técnicas de trabajo vertical y descenso controlado por cuerdas (rapel). Los operarios, debidamente equipados con arneses y sistemas de seguridad, realizaron trabajos en diferentes alturas y zonas de difícil acceso, interviniendo columnas, paredes, molduras, cornisas y demás elementos arquitectónicos exteriores, incluyendo las áreas cercanas a los grandes ventanales de vidrio. Las labores se desarrollaron progresivamente desde los niveles superiores hasta las áreas inferiores del edificio, protegiendo previamente las zonas de la planta baja mediante lonas y materiales de cobertura para evitar manchas o daños durante la ejecución, logrando como resultado la renovación y uniformidad del acabado exterior, contribuyendo a la conservación y mejora estética de la fachada del edificio.",
+    category: "Pintura de Edificios",
+    visible: true,
+    images: [
+      "/ast 1.jpeg",
+      "/ast 2.jpeg",
+      "/ast 3.jpeg",
+      "/ast 4.jpeg",
+      "/ast 5.jpeg"
+    ]
+    },
+    {
+    id: "26",
+    title: "Pintura exterior en el Edificio Nobis",
+    location: "Avenida Joaquín Orrantia y la Avenida Juan Tanca Marengo.",
+    date: "25 de noviembre, 2022",
+    description: " Se realizó el mantenimiento de la pintura de la fachada en el Edificio Nobis, una moderna torre de varios pisos con una estructura curva de franjas horizontales alternadas entre grandes ventanales de vidrio azul y paneles revestidos. El trabajo consistió en la renovación del recubrimiento exterior mediante la aplicación de pintura de tonos claros, ejecutada en los niveles superiores y verticales del edificio para conservar su aspecto impecable y corporativo. ",
+    category: "Pintura de Edificios",
+    visible: true,
+    images: [
+      "/nob 1.jpeg",
+      "/nob 2.jpeg",
+      "/nob 3.jpeg",
+      "/nob 4.jpeg",
+      "/nob 5.jpeg"
+    ]
+    },
+    {
+    id: "27",
+    title: "Pintura exterior en Condominio",
+    location: "Kenedy Norte.",
+    date: "25 de enero, 2023",
+    description: " Se realizó el mantenimiento de la pintura de la fachada de este condominio ubicado en Kennedy Norte, un edificio residencial de varias plantas con balcones y una estructura frontal de portones metálicos negros. El trabajo consistió en la renovación del recubrimiento exterior de las paredes y detalles arquitectónicos, aplicando pintura de tonos claros para revitalizar la imagen de la edificación. ",
+    category: "Pintura de Edificios",
+    visible: true,
+    images: [
+      "/ken 1.jpeg",
+      "/ken 2.jpeg",
+      "/ken 3.jpeg",
+      "/ken 4.jpeg"
+    ]
+    },
+        {
+    id: "27",
+    title: "Pintura exterior en Condominio",
+    location: "Kenedy Norte.",
+    date: "25 de enero, 2023",
+    description: " Se realizó el mantenimiento de la pintura de la fachada de este condominio ubicado en Kennedy Norte, un edificio residencial de varias plantas con balcones y una estructura frontal de portones metálicos negros. El trabajo consistió en la renovación del recubrimiento exterior de las paredes y detalles arquitectónicos, aplicando pintura de tonos claros para revitalizar la imagen de la edificación. ",
+    category: "Pintura de Edificios",
+    visible: true,
+    images: [
+      "/ken 1.jpeg",
+      "/ken 2.jpeg",
+      "/ken 3.jpeg",
+      "/ken 4.jpeg"
+    ]
+    },
 ];
